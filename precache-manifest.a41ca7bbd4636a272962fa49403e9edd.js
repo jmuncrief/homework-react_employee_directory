08b6f9432cb4d7c930c79546342289827dd4914f
Jordan Muncrief <jordean89@gmail.com>
2020-10-18 08:42:44 -0600

    Updates

---
+++ precache-manifest.a41ca7bbd4636a272962fa49403e9edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089be542f4a286bae6025b4f9b466d6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06091e6e775d2a71b699",
+    "url": "/static/css/main.cfb4800e.chunk.css"
+  },
+  {
+    "revision": "4c1dee0635f8e7df5cf7",
+    "url": "/static/js/2.0c3b5517.chunk.js"
+  },
+  {
+    "revision": "32d705db082c23381aab0b371144a26f",
+    "url": "/static/js/2.0c3b5517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06091e6e775d2a71b699",
+    "url": "/static/js/main.605bb099.chunk.js"
+  },
+  {
+    "revision": "1a219a8ad6be429a361a",
+    "url": "/static/js/runtime-main.5188f4d0.js"
+  }
+]);
